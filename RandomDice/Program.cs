761e7a80f03ae974dccf7a5c28b4132b87f06bf6
TomasCarvalho <2606tom@gmail.com>
2023-03-29 23:46:49 +0100

    Create testing version of RandomDice

---
+++ RandomDice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random die = new Random();
+            int n = int.Parse(Console.ReadLine());
+            int sum;
+
+            // for (int i = 0 ; i < n ; i++)
+            // {
+            //     sum += die.Next(1, 7);
+            // }
+            sum = die.Next(1.7);
+            Console.WriteLine($"Dice add up to {sum}.");
+
+
+
+                
+        }
+    }
+}
