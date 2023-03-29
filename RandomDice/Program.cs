b1d6ced1925b3bdd8514ba43ffe27c3a52c5d778
TomasCarvalho <2606tom@gmail.com>
2023-03-29 23:54:53 +0100

    Finish implementing project RandomDice

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -7,19 +7,17 @@ namespace RandomDice
         static void Main(string[] args)
         {
             Random die = new Random();
+            int sum = 0;
+            Console.WriteLine("Input # of dice to roll:");
             int n = int.Parse(Console.ReadLine());
-            int sum;
+            
 
-            // for (int i = 0 ; i < n ; i++)
-            // {
-            //     sum += die.Next(1, 7);
-            // }
-            sum = die.Next(1.7);
-            Console.WriteLine($"Dice add up to {sum}.");
+            for (int i = 0 ; i < n ; i++)
+            {
+                sum += die.Next(1, 7);
+            }
 
-
-
-                
+            Console.WriteLine($"Dice add up to {sum}.");   
         }
     }
 }
