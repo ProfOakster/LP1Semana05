24b699649e40d3b36b76ef22250d24c1a269476e
TomasCarvalho <2606tom@gmail.com>
2023-03-31 11:33:44 +0100

    Add code comments

--- UpperUnion/Program.cs
+++ UpperUnion/Program.cs
@@ -7,21 +7,28 @@ namespace UpperUnion
     {
         static void Main(string[] args)
         {
+            //Check if arguments were given
             if (args.Length > 0)
             {
+                //Declares new string array
                 string[] inp = new string[args.Length];
 
+                //Turns all arguments to uppercase and stores them in inp
                 for (int i = 0; i < args.Length; i++)
                     inp[i] = args[i].ToUpper();
 
+                //Sorts strings in array by alphabetical order
                 Array.Sort(inp);
 
+                //Unites all strings in res in a StringBuilder, connected by "-"'s
                 StringBuilder res = new StringBuilder();
                 res.AppendJoin("-",inp);
- 
+
+                //Displays result
                 Console.WriteLine(res.ToString());
 
             }
+            //If no arguments are given
             else
                 Console.WriteLine("NO ARGUMENTS");
 
