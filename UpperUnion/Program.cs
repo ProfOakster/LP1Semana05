7a8aa33d793d2ca884a971b55ebe69393c350e9f
TomasCarvalho <2606tom@gmail.com>
2023-03-31 10:41:04 +0100

    Create project UpperUnion; Implement args uppercase conversion

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UpperUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string[] res = new string[args.Length];
+
+                for (int i = 0; i < args.Length; i++)
+                    res[i] = args[i].ToUpper();
+
+
+
+
+
+
+
+                for (int i = 0; i < res.Length; i++)
+                    Console.WriteLine(res[i]);
+            }
+            else
+                Console.WriteLine("NO ARGUMENTS");
+
+
+
+
+            
+        }
+    }
+}
